4dbefa03daf1c5d31f89babb2709a1f500d55dbd
Jaume Riera <jaumerg95@gmail.com>
2023-04-09 18:51:46 +0200

    Import package photon and create scene transition

--- Assets/Scripts/ConnectingToServer.cs
+++ Assets/Scripts/ConnectingToServer.cs
@@ -2,10 +2,20 @@ using AOT;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class ConnectingToServer : MonoBehaviour
+public class ConnectingToServer : MonoBehaviourPunCallbacks
 {
     private void Start() {
-        PhotonNetwork.ConnectUsingSettings("Foo");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby() {
+        SceneManager.LoadScene("Lobby");
     }
 }
